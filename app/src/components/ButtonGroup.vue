96c1d1eaadfd9deb6b51c3c2d5d32a39d162c423
yyh1102 <yyh951102@163.com>
2018-12-09 18:58:01 +0800

    Add app frontend simple demo. Add eos contract boilerplate

---
+++ app/src/components/ButtonGroup.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="group">
+    <div>
+      <md-button class="md-raised md-primary">发起提案</md-button>
+    </div>
+    <div>
+      <md-button class="md-raised">前往仓库</md-button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ButtonGroup"
+  }
+</script>
+
+<style scoped>
+  .group{
+    text-align: left;
+    padding-top:50px;
+    padding-left:40px;
+  }
+</style>
---
+++ app/src/views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="home">
+    <!--<img alt="Vue logo" src="../assets/logo.png">-->
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <div class="container">
+      <div class="main">
+        <propose-list>
+
+        </propose-list>
+      </div>
+      <div class="right">
+        <button-group></button-group>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .container {
+    margin: 0 auto;
+    width: 800px;
+    display: flex;
+    flex-direction: row;
+  }
+
+  .main {
+    flex: 2.5;
+  }
+
+  .right {
+    flex: 1;
+  }
+</style>
+
+<script>
+  // @ is an alias to /src
+  import ProposeList from '@/components/ProposeList.vue'
+  import ButtonGroup from '@/components/ButtonGroup.vue'
+
+  export default {
+    name: 'home',
+    components: {
+      ProposeList,
+      ButtonGroup
+    }
+  }
+</script>
