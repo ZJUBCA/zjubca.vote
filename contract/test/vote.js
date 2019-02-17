9636d6322ed762a145ea13c536c08d2a6b51f91d
yyh1102 <yyh951102@163.com>
2019-02-18 01:04:17 +0800

    modify vote test

--- contract/test/vote.js
+++ contract/test/vote.js
@@ -19,47 +19,47 @@ describe("vote contract test", function () {
 
     /* ===== before the test, zjubcatokent has issued 1000.0000 ZJUBCA to eosio ===== */
 
-    it('should success eosio set pro vote lower than balance to issue 10', async function () {
-        let processed = await vote.setvote("eosio", 0, 10, "10.0000 ZJUBCA")
+    it('should success eosio set pro vote lower than balance to issue 1', async function () {
+        let processed = await vote.setvote("eosio", 0, 1, "10.0000 ZJUBCA")
         assert.equal(Js4eos.okTransaction(processed), true)
     });
 
-    it('should success eosio set con vote (lower than balance) to issue 20', async function () {
-        let processed = await vote.setvote("eosio", 1, 20, "10.0000 ZJUBCA")
+    it('should success eosio set con vote (lower than balance) to issue 2', async function () {
+        let processed = await vote.setvote("eosio", 1, 2, "10.0000 ZJUBCA")
         assert.equal(Js4eos.okTransaction(processed), true)
     })
 
-    it('should fail eosio set pro vote larger than balance to issue 10', async function () {
-        let processed = await vote.setvote("eosio", 0, 10, "100000000.0000 ZJUBCA")
+    it('should fail eosio set pro vote larger than balance to issue 1', async function () {
+        let processed = await vote.setvote("eosio", 0, 1, "100000000.0000 ZJUBCA")
         assert.equal(Js4eos.okTransaction(processed), false)
     });
 
-    it('should success eosio withdraw pro vote from issue 10', async function () {
-        let processed = await vote.withdraw("eosio", 10)
+    it('should success eosio withdraw pro vote from issue 1', async function () {
+        let processed = await vote.withdraw("eosio", 1);
         assert.equal(Js4eos.okTransaction(processed), true)
     });
 
-    it('should success zjubcavote11 close issue 10 with `pass` state', async function () {
+    it('should success zjubcavote11 close issue 1 with `pass` state', async function () {
         await vote.setActor("zjubcavote11")
-        let processed = await vote.close(10, 0)
+        let processed = await vote.close(1, 0)
         assert.equal(Js4eos.okTransaction(processed), true)
     });
 
-    it('should fail zjubcavote11 close issue 10 with `not pass` state', async function () {
+    it('should fail zjubcavote11 close issue 1 with `not pass` state', async function () {
         await vote.setActor("zjubcavote11")
-        let processed = await vote.close(10, 1)
+        let processed = await vote.close(1, 1)
         assert.equal(Js4eos.okTransaction(processed), false)
     });
 
-    it('should success zjubcavote11 reopen issue 10', async function () {
+    it('should success zjubcavote11 reopen issue 1', async function () {
         await vote.setActor("zjubcavote11")
-        let processed = await vote.reopen(10)
+        let processed = await vote.reopen(1)
         assert.equal(Js4eos.okTransaction(processed), true)
     });
 
-    it('should fail zjubcavote11 reopen issue 20', async function () {
+    it('should fail zjubcavote11 reopen issue 2', async function () {
         await vote.setActor("zjubcavote11")
-        let processed = await vote.reopen(20)
+        let processed = await vote.reopen(2)
         assert.equal(Js4eos.okTransaction(processed), false)
     });
 });
